3acc5444f90fe0a30c40e3cd8d9168a35456868f
AnThanh1423 <thanhan1423@gmail.com>
2022-04-03 00:44:30 +0700

    aaa

---
+++ ls01/script.js
@@ -0,0 +1,42 @@
+// function logger(a) {
+//     console.log(`hello ${a}`)
+// }
+// logger("an");
+
+//function có giá trị trả vè
+
+//arrow function
+// const logger = (name) => {
+//     console.log(`hello ${name}`);
+// }
+// logger('an');
+
+//object
+//ket và value
+// let person = {
+//     firstName: 'Nguyễn Thanh',
+//     lastName :"An"
+// };
+// // let firstName = person.firstName;
+// // let lastName = person.lastName;
+// // console.log(firstName,lastName);
+
+// const { firstName:newName,lastName,age} = person;
+// console.log(newName, lastName, age)
+
+// let fullName = "Thanh An";
+// let age = 18;
+
+// let person = { fullName, age }
+// console.log(person)
+
+// let number = [1, 2, 3, 4, 5];
+
+// let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+// console.log(number[2])
+
+// import {sum,log } from "./math.js"
+// console.log(sum(1, 2))
+// log();
+// import Pi from "./math.js"
+// console.log(Pi)
